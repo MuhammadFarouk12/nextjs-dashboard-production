36610ffed5ca9347d13b0affc0d363b9af104608
MuhammadFarouk12 <muhammad12fk@gmail.com>
2026-02-18 17:25:22 +0300

    test

--- app/dashboard/invoices/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Invoices(){
-	return <p>Customers Page</p>
-}
--- app/ui/fonts.ts
+++
@@ -1,5 +0,0 @@
-import { Cairo } from 'next/font/google'
-import { Lusitana } from "next/font/google"
-
-export const cairo = Cairo({ subsets: ['latin']})
-export const lusitana  = Lusitana({weight: "400"})
