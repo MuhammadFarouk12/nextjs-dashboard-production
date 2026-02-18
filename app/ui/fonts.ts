4c2ec20f6643db61456eea648ece1a925f1c9e50
MuhammadFarouk12 <muhammad12fk@gmail.com>
2026-02-18 15:27:38 +0300

    add static

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Cairo } from 'next/font/google'
+import { Lusitana } from "next/font/google"
+
+export const cairo = Cairo({ subsets: ['latin']})
+export const lusitana  = Lusitana({weight: "400"})
